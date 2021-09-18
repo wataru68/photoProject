e021b88232e2e970b0f62c0808df281b77b2dd10
unknown <nishidawataru12@gmail.com>
2021-09-18 20:31:23 +0900

    132行目~140行目をコメントアウト

--- photoProject/settings.py
+++ photoProject/settings.py
@@ -121,6 +121,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Userモデルの代わりにCustomUserモデルを使用するための設定
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+# mediaフォルダーの場所(BASE_DIR以下のmedia)を登録
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# mediaのURLを登録
+MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+"""
 # メール送信のためのクラスを設定
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # メールサーバーへの接続設定
@@ -130,8 +138,4 @@ EMAIL_POST = 587
 EMAIL_HOST_USER = 'nishidawataru12@gmail.com'
 EMAIL_HOST_PASSWORD = 'vnsb vhgy bmbq piaf'
 EMAIL_USE_TLS = True
-
-# mediaフォルダーの場所(BASE_DIR以下のmedia)を登録
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# mediaのURLを登録
-MEDIA_URL = '/media/'
+"""
